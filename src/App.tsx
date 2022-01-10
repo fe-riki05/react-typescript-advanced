34f6e4b8e75d680fd709f7382eea4705271e3642
RIKI <ab.jpnet.01@gmail.com>
2022-01-10 19:41:45 +0900

    props 型

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
+import { UserProfile } from "./UserProfile";
+
+import { Text } from "./Text";
+
+const user: User = {
+  name: "じゃけぇ"
+  // hobbies: ["映画", "ゲーム"]
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -16,6 +24,8 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
+      <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo
